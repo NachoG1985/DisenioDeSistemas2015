f069066e59a0cbb5cc279bbd2b180c6c2453a89b
Santiago Cogorno <santiago4322@gmail.com>
2015-08-24 16:15:04 -0300

    Agrego insertarUsuario e insertarReceta
    insertarUsuario funca, falta ver bien insertarReceta el enum

--- Que comemos hoy/src/clases/ConsultorBaseDeDatos.java
+++ Que comemos hoy/src/clases/ConsultorBaseDeDatos.java
@@ -3,6 +3,12 @@ package clases;
 import java.sql.*;
 
 
+
+enum temporada { 
+    Invierno,Verano,Otonio,Primavera
+}
+
+
 public class ConsultorBaseDeDatos {
 
 	// Atributos de la clase
@@ -115,6 +121,71 @@ public class ConsultorBaseDeDatos {
         }        return resultado;
     }
 	
+	//Inserta un Usuario y lo muestra
+	public void insertarUsuario(String nombre,String mail,String fecha,String pass) {
+	 	ResultSet data;
+	 	Connection cn = null;
+	 	CallableStatement cst = null;
+        try {
+        	 cn = getConexion("disenio", "root", "");
+           	           
+             cst = cn.prepareCall("{call insertarUsuario(?,?,?,?)}");
+             cst.setString(1,nombre);
+             cst.setString(2,mail);
+             cst.setString(3,fecha);
+             cst.setString(4,pass);
+             cst.executeUpdate();
+             cst = cn.prepareCall("{call test(?)}");
+             cst.setString(1,nombre);
+             data = cst.executeQuery();
+             if (data != null) 
+             {
+            	 System.out.println(" usuario_id      nombreUsuario      email      fechaNac      contrasenia");
+                 System.out.println("--------------------------------");
+                 while (data.next())
+                	{
+                    System.out.println(""+data.getInt("usuario_id")+"    "+data.getString("nombreUsuario")+"    "+data.getString("email")+"    "+data.getString("fechaNac")+"    "+data.getString("contrasenia"));
+                	}                 
+                
+             }else{System.out.println("No existe usuario con ese nombre");}
+            
+        }catch (Exception e) {
+        	
+        }
+ }  
+ 
+	 public void insertarReceta(String nombre,int dificultad,temporada temporada,int calorias) {
+		 	ResultSet data;
+		 	Connection cn = null;
+		 	CallableStatement cst = null;
+	        try {
+	        	 cn = getConexion("disenio", "root", "");
+	           	           
+	             cst = cn.prepareCall("{call insertarReceta(?,?,?,?)}");
+	             cst.setString(1,nombre);
+	             cst.setInt(2,dificultad);
+	             cst.setString(3,temporada.name());
+	             cst.setInt(4,calorias);
+	             cst.executeUpdate();
+	             cst = cn.prepareCall("{call test(?)}");
+	             cst.setString(1,nombre);
+	             data = cst.executeQuery();
+	             if (data != null) 
+	             {
+	            	 System.out.println(" recetas_id      nombre      dificultad      temporada      calorias");
+	                 System.out.println("--------------------------------");
+	                 while (data.next())
+	                	{
+	                    System.out.println(""+data.getInt("recetas_id")+"    "+data.getString("nombre")+"    "+data.getInt("dificultad")+"    "+data.getString("temporada")+"    "+data.getInt("calorias"));
+	                	}                 
+	                
+	             }else{System.out.println("No existe usuario con ese nombre");}
+	            
+	        }catch (Exception e) {
+	        	
+	        }
+	 }  
+	 
 	
     public void desconectar()
     {
