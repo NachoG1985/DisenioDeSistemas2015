bfa4db498984ec5c1f0cf215a136d9d204a34604
Santiago Cogorno <santiago4322@gmail.com>
2015-10-10 22:39:02 -0300

    DB Modificada

--- Base de datos/disenio.sql
+++ Base de datos/disenio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2015 a las 17:35:57
+-- Tiempo de generación: 11-10-2015 a las 02:13:45
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -160,20 +160,8 @@ CREATE TABLE IF NOT EXISTS `calificacion de un usuario a receta` (
   `calif_recet_usuario` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `recetas_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
-  `calificaciones_id` int(11) NOT NULL,
+  `calificacion` int(11) NOT NULL,
   PRIMARY KEY (`calif_recet_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `calificaciones`
---
-
-CREATE TABLE IF NOT EXISTS `calificaciones` (
-  `calificaciones_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `calificacion` enum('0','0.5','1','1.5','2','2.5','3','3.5','4','4.5','5') COLLATE utf8_spanish2_ci NOT NULL,
-  PRIMARY KEY (`calificaciones_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -201,6 +189,18 @@ CREATE TABLE IF NOT EXISTS `categorias` (
   `horaMax` double NOT NULL,
   `horaMin` double NOT NULL,
   PRIMARY KEY (`categorias_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `condicionprexistente`
+--
+
+CREATE TABLE IF NOT EXISTS `condicionprexistente` (
+  `condicion_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`condicion_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -253,25 +253,14 @@ INSERT INTO `condimentos de una receta` (`condim_recet_id`, `recetas_id`, `condi
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `consultareceta`
+-- Estructura de tabla para la tabla `dietas`
 --
 
-CREATE TABLE IF NOT EXISTS `consultareceta` (
-  `consulta_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `fecha` timestamp NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `recetas_id` int(11) NOT NULL,
-  PRIMARY KEY (`consulta_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=4 ;
-
---
--- Volcado de datos para la tabla `consultareceta`
---
-
-INSERT INTO `consultareceta` (`consulta_id`, `fecha`, `usuario_id`, `recetas_id`) VALUES
-(1, '2015-09-15 05:15:16', 48, 10),
-(2, '2015-09-12 22:33:21', 53, 12),
-(3, '2015-09-12 22:35:04', 45, 9);
+CREATE TABLE IF NOT EXISTS `dietas` (
+  `dieta_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`dieta_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -284,7 +273,7 @@ CREATE TABLE IF NOT EXISTS `historial` (
   `usuario_id` int(10) NOT NULL,
   `receta_id` int(10) NOT NULL,
   `operacion` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `tiempo` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tiempo` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`movimiento_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
 
@@ -299,7 +288,7 @@ CREATE TABLE IF NOT EXISTS `ingredientes` (
   `nombre` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
   `porcion` int(11) NOT NULL,
   `calorias` int(11) NOT NULL,
-  `nivelAlimenticio` int(11) NOT NULL,
+  `nivel_id` int(11) NOT NULL,
   PRIMARY KEY (`ingredientes_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=5 ;
 
@@ -307,11 +296,11 @@ CREATE TABLE IF NOT EXISTS `ingredientes` (
 -- Volcado de datos para la tabla `ingredientes`
 --
 
-INSERT INTO `ingredientes` (`ingredientes_id`, `nombre`, `porcion`, `calorias`, `nivelAlimenticio`) VALUES
-(1, 'harina', 100, 250, 1),
-(2, 'gelatina instantanea', 50, 70, 2),
-(3, 'carne', 250, 500, 4),
-(4, 'queso', 150, 300, 1);
+INSERT INTO `ingredientes` (`ingredientes_id`, `nombre`, `porcion`, `calorias`, `nivel_id`) VALUES
+(1, 'harina', 100, 250, 0),
+(2, 'gelatina instantanea', 50, 70, 0),
+(3, 'carne', 250, 500, 0),
+(4, 'queso', 150, 300, 0);
 
 -- --------------------------------------------------------
 
@@ -337,6 +326,18 @@ INSERT INTO `ingredientes de una receta` (`ingred_recet_id`, `recetas_id`, `ingr
 (3, 14, 1, 0),
 (4, 11, 1, 100),
 (5, 9, 4, 300);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nivelalimenticio`
+--
+
+CREATE TABLE IF NOT EXISTS `nivelalimenticio` (
+  `nivel_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`nivel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -372,8 +373,8 @@ CREATE TABLE IF NOT EXISTS `perfil usuario` (
   `edad` int(3) NOT NULL,
   `altura` double NOT NULL,
   `complexion` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `dieta` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `rutina` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `dieta_id` int(11) NOT NULL,
+  `rutina_id` int(11) NOT NULL,
   PRIMARY KEY (`perfil_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=7 ;
 
@@ -381,11 +382,11 @@ CREATE TABLE IF NOT EXISTS `perfil usuario` (
 -- Volcado de datos para la tabla `perfil usuario`
 --
 
-INSERT INTO `perfil usuario` (`perfil_id`, `nombre`, `apellido`, `sexo`, `edad`, `altura`, `complexion`, `dieta`, `rutina`) VALUES
-(1, 'Juan', 'Reines', 'Masculino', 20, 1.7, '22.04', 'Normal', 'Nada'),
-(2, 'Franco', 'Abregu', 'Masculino', 20, 1.68, '20.01', 'Vegano', 'Activo'),
-(3, 'Nacho', 'ghillini', 'Masculino', 28, 1.72, '30', 'Ovolactovegetariano', 'Mediano'),
-(6, 'Roman', 'Riquelme', 'Masculino', 25, 1.78, 'Mediana', 'Normal', 'Intenso');
+INSERT INTO `perfil usuario` (`perfil_id`, `nombre`, `apellido`, `sexo`, `edad`, `altura`, `complexion`, `dieta_id`, `rutina_id`) VALUES
+(1, 'Juan', 'Reines', 'Masculino', 20, 1.7, '22.04', 0, 0),
+(2, 'Franco', 'Abregu', 'Masculino', 20, 1.68, '20.01', 0, 0),
+(3, 'Nacho', 'ghillini', 'Masculino', 28, 1.72, '30', 0, 0),
+(6, 'Roman', 'Riquelme', 'Masculino', 25, 1.78, 'Mediana', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -395,7 +396,7 @@ INSERT INTO `perfil usuario` (`perfil_id`, `nombre`, `apellido`, `sexo`, `edad`,
 
 CREATE TABLE IF NOT EXISTS `preferencias alimenticias de usuario` (
   `prefe_usuario_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `recetas_id` int(11) NOT NULL,
+  `ingredientes_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   PRIMARY KEY (`prefe_usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
@@ -445,16 +446,14 @@ CREATE TABLE IF NOT EXISTS `recetas` (
   `nombre` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
   `ingrediente_ppal` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `dificultad` int(4) NOT NULL,
-  `temporada` varchar(15) COLLATE utf8_spanish2_ci NOT NULL,
+  `temporada_id` int(11) NOT NULL,
   `categoria1` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `categoria2` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `categoria3` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `categoria4` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `caloriasTotales` int(11) NOT NULL,
-  `creador` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `dia` int(11) NOT NULL,
-  `mes` int(11) NOT NULL,
-  `anio` int(11) NOT NULL,
+  `condicion_id` int(11) NOT NULL,
+  `dieta_id` int(11) NOT NULL,
   PRIMARY KEY (`recetas_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=25 ;
 
@@ -462,55 +461,16 @@ CREATE TABLE IF NOT EXISTS `recetas` (
 -- Volcado de datos para la tabla `recetas`
 --
 
-INSERT INTO `recetas` (`recetas_id`, `nombre`, `ingrediente_ppal`, `dificultad`, `temporada`, `categoria1`, `categoria2`, `categoria3`, `categoria4`, `caloriasTotales`, `creador`, `dia`, `mes`, `anio`) VALUES
-(8, 'milanesa a la maryland', 'milanesa', 3, 'Invierno', 'cena', 'almuerzo', NULL, NULL, 541, '', 0, 0, 0),
-(9, 'pizza', 'harina', 1, 'Verano', 'cena', NULL, NULL, NULL, 125, '', 0, 0, 0),
-(10, 'pati a la parrilla', 'carne', 5, 'Verano', 'cena', 'almuerzo', NULL, NULL, 500, '', 0, 0, 0),
-(11, 'tallarines', 'harina', 2, 'Invierno', NULL, NULL, NULL, NULL, 350, '', 0, 0, 0),
-(12, 'buñuelos de acelga', 'acelga', 2, 'Invierno', 'almuerzo', NULL, NULL, NULL, 110, '', 0, 0, 0),
-(13, 'pastel de papa', 'papa', 3, 'Invierno', 'cena', NULL, NULL, NULL, 110, '', 0, 0, 0),
-(14, 'bizcocho de vainilla', 'harina', 2, 'Otonio', 'merienda', NULL, NULL, NULL, 420, '', 0, 0, 0),
-(20, 'cafe', 'cafe instantaneo', 1, 'Invierno', 'desayuno', NULL, NULL, NULL, 514, '', 0, 0, 0),
-(24, 'gelatina', 'gelatina instantanea', 2, 'Invierno', 'desayuno', NULL, NULL, NULL, 514, 'roman', 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `recetas calificadas`
---
-
-CREATE TABLE IF NOT EXISTS `recetas calificadas` (
-  `recet_calificadas_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `recetas_id` int(11) NOT NULL,
-  PRIMARY KEY (`recet_calificadas_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `recetas confirmadas`
---
-
-CREATE TABLE IF NOT EXISTS `recetas confirmadas` (
-  `recet_confirmadas_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `recetas_id` int(11) NOT NULL,
-  PRIMARY KEY (`recet_confirmadas_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `recetas consultadas`
---
-
-CREATE TABLE IF NOT EXISTS `recetas consultadas` (
-  `recet_consultadas_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `recetas_id` int(11) NOT NULL,
-  PRIMARY KEY (`recet_consultadas_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
+INSERT INTO `recetas` (`recetas_id`, `nombre`, `ingrediente_ppal`, `dificultad`, `temporada_id`, `categoria1`, `categoria2`, `categoria3`, `categoria4`, `caloriasTotales`, `condicion_id`, `dieta_id`) VALUES
+(8, 'milanesa a la maryland', 'milanesa', 3, 0, 'cena', 'almuerzo', NULL, NULL, 541, 0, 0),
+(9, 'pizza', 'harina', 1, 0, 'cena', NULL, NULL, NULL, 125, 0, 0),
+(10, 'pati a la parrilla', 'carne', 5, 0, 'cena', 'almuerzo', NULL, NULL, 500, 0, 0),
+(11, 'tallarines', 'harina', 2, 0, NULL, NULL, NULL, NULL, 350, 0, 0),
+(12, 'buñuelos de acelga', 'acelga', 2, 0, 'almuerzo', NULL, NULL, NULL, 110, 0, 0),
+(13, 'pastel de papa', 'papa', 3, 0, 'cena', NULL, NULL, NULL, 110, 0, 0),
+(14, 'bizcocho de vainilla', 'harina', 2, 0, 'merienda', NULL, NULL, NULL, 420, 0, 0),
+(20, 'cafe', 'cafe instantaneo', 1, 0, 'desayuno', NULL, NULL, NULL, 514, 0, 0),
+(24, 'gelatina', 'gelatina instantanea', 2, 0, 'desayuno', NULL, NULL, NULL, 514, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -542,6 +502,31 @@ INSERT INTO `recetas de un usuario` (`recet_usuario_id`, `usuario_id`, `recetas_
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `rutinas`
+--
+
+CREATE TABLE IF NOT EXISTS `rutinas` (
+  `rutina_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `calorias` float NOT NULL,
+  PRIMARY KEY (`rutina_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temporadas`
+--
+
+CREATE TABLE IF NOT EXISTS `temporadas` (
+  `temporada_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`temporada_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -550,9 +535,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `nombreUsuario` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `contrasenia` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
-  `dia` int(11) NOT NULL,
-  `mes` int(11) NOT NULL,
-  `anio` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`usuario_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=54 ;
 
@@ -560,17 +543,17 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`usuario_id`, `nombreUsuario`, `email`, `contrasenia`, `dia`, `mes`, `anio`) VALUES
-(4, 'juanchoPebete', 'juan_ellince@gmail.com', 'cherry', 0, 0, 0),
-(7, 'franquitoMaquinola', 'franco42@gmail.com', 'abreu', 0, 0, 0),
-(45, 'roman', 'roman@gmail.com', 'riquelme', 0, 0, 0),
-(46, 'nacho', 'nacho45@yahoo.com', 'calafate', 0, 0, 0),
-(48, 'rodrigo', 'lince@telecentro.com', 'vagoneta', 0, 0, 0),
-(49, 'santi', 'santi123@hotmail.com', 'aguanteFS', 0, 0, 0),
-(50, 'oscarcito', 'oscar_seguime@twitter.com', 'jefeCatedra', 0, 0, 0),
-(51, 'tito', 'oscar_seguime@twitter.com', 'jefeCatedra', 0, 0, 0),
-(52, 'pepe', 'casas@gmail.com', 'llamamepepe', 0, 0, 0),
-(53, 'matiGrippo', 'estaEnMedrano@yahoo.com', 'error404', 24, 4, 1994);
+INSERT INTO `usuarios` (`usuario_id`, `nombreUsuario`, `email`, `contrasenia`, `fecha`) VALUES
+(4, 'juanchoPebete', 'juan_ellince@gmail.com', 'cherry', '2015-10-10 21:09:15'),
+(7, 'franquitoMaquinola', 'franco42@gmail.com', 'abreu', '2015-10-10 21:09:15'),
+(45, 'roman', 'roman@gmail.com', 'riquelme', '2015-10-10 21:09:15'),
+(46, 'nacho', 'nacho45@yahoo.com', 'calafate', '2015-10-10 21:09:15'),
+(48, 'rodrigo', 'lince@telecentro.com', 'vagoneta', '2015-10-10 21:09:15'),
+(49, 'santi', 'santi123@hotmail.com', 'aguanteFS', '2015-10-10 21:09:15'),
+(50, 'oscarcito', 'oscar_seguime@twitter.com', 'jefeCatedra', '2015-10-10 21:09:15'),
+(51, 'tito', 'oscar_seguime@twitter.com', 'jefeCatedra', '2015-10-10 21:09:15'),
+(52, 'pepe', 'casas@gmail.com', 'llamamepepe', '2015-10-10 21:09:15'),
+(53, 'matiGrippo', 'estaEnMedrano@yahoo.com', 'error404', '2015-10-10 21:09:15');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
